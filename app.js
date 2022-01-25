c27ec47eb94c8fb74d5521e97cb6f30c69822864
Nazar <doskoch25@gmail.com>
2022-01-25 14:54:56 +0200

    final

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const {v4} = require('uuid')
 
 
+let CONTACTS = [
+    {id: v4(), name: 'Nazar', value: '380-63-45-10-555', marked: false},
+    {id: v4(), name: 'Nazar', value: '380-63-45-10-555', marked: false},
+]
 
+app.use(express.json())
 
+//GET
+app.get('/api/contacts', (req, res) => {
 
+    // const contact = {req}
+    res.status(200).json(CONTACTS)
 
-app.use(express.static(path.resolve(__dirname,'client')))
+})
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.resolve('client','index.html',))
+//POST
+
+app.post('/api/contacts/', (req, res) => {
+
+    const contact = {...req.body, id: v4(), marked: false}
+    // console.log(req.body.name)
+    // res.json({...req.body})
+    CONTACTS.push(contact)
+    res.status(201).json(contact)
+})
+
+//DELETE
+
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+    res.status(200).json({message: 'Контак удален'})
+})
+
+//PUT
+app.put('/api/contacts/:id', (req, res) => {
+    // res.json({...req.body})
+    // res.json(req.params)
+    const idx = CONTACTS.findIndex(c=>c.id === req.params.id)
+    CONTACTS[idx] = req.body
+    res.json(CONTACTS[idx])
+})
+
+//=================
+app.use(express.static(path.resolve(__dirname, 'client')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'index.html'))
 })
 
 app.listen(3000, () => console.log('Server has been started on port 3000...'))
--- client/index.js
+++ client/index.js
@@ -1,3 +1,90 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
 
-console.log(Vue)
+Vue.component('loader', {
+    template: `
+    <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+    `
+})
+
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            loading: false,
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: [
+                /*{id: 1, name: 'Nazar', value: '380-63-45-10-555', marked: false}*/
+            ],
+            testArr: [1, 2, 3, 5]
+        }
+    },
+    computed: {
+        canCreate() {
+            return this.form.value.trim() && this.form.name.trim()
+        }
+    },
+    methods: {
+        async createContacts() {
+            const {...contact} = this.form
+
+            const newContact = await request('/api/contacts', 'POST', contact)
+
+            console.log('newContact', newContact)
+
+            this.contacts.push(newContact)
+
+            // this.contacts.push({...contact, id: Date.now(), marked: false})
+            // this.form.name = this.form.value = ''
+        },
+        async markContact(id) {
+
+            const contact = this.contacts.find(c => c.id === id)
+
+            const updated = await request(`/api/contacts/${id}`, 'PUT', {
+                ...contact,
+                marked: true
+            })
+
+            console.log(updated)
+
+            contact.marked = true
+        },
+        async removeContact(id) {
+            await request(`/api/contacts/${id}`, 'DELETE')
+            this.contacts = this.contacts.filter(c => c.id !== id)
+        }
+    },
+    async mounted() {
+        this.loading = true
+        const data = await request('/api/contacts')
+        this.contacts = data
+        this.loading = false
+        console.log(data)
+    }
+})
+
+async function request(url, method = 'GET', data = null) {
+    try {
+        const headers = {}
+        let body
+
+        if (data) {
+            headers['Content-Type'] = 'application/json'
+            body = JSON.stringify(data)
+        }
+
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        })
+        return response.json()
+    } catch (e) {
+        console.warn('Error', e.message)
+    }
+}
